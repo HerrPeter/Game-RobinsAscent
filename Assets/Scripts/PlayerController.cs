9a58c59d633885948f4b394283c12c16c180fd72
Peter (PLT-2) <peterkaufinger@gmail.com>
2026-03-18 09:09:10 -0700

    added flip of player sprite when dir change

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,60 +3,72 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 6f;
-    public float jumpForce = 12f;
-    public float fallThreshold = 6f;
-    public float wrapLeft = -3.5f;
-    public float wrapRight = 3.5f;
+  public float moveSpeed = 6f;
+  public float jumpForce = 12f;
+  public float fallThreshold = 6f;
+  public float wrapLeft = -3.5f;
+  public float wrapRight = 3.5f;
 
-    private Rigidbody2D rb;
-    private float moveInput;
-    private GameManager gameManager;
+  private Rigidbody2D rb;
+  private float moveInput;
+  private GameManager gameManager;
+  private SpriteRenderer sr;
 
-    void Start()
+  void Start()
+  {
+    rb = GetComponent<Rigidbody2D>();
+    gameManager = FindFirstObjectByType<GameManager>();
+    sr = GetComponent<SpriteRenderer>();
+  }
+
+  void Update()
+  {
+    if (Keyboard.current != null)
     {
-        rb = GetComponent<Rigidbody2D>();
-        gameManager = FindFirstObjectByType<GameManager>();
+      moveInput = 0f;
+
+      if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+        moveInput = -1f;
+
+      if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+        moveInput = 1f;
     }
 
-    void Update()
+    rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+    if (transform.position.y < Camera.main.transform.position.y - fallThreshold)
     {
-        if (Keyboard.current != null)
-        {
-            moveInput = 0f;
-
-            if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
-                moveInput = -1f;
-
-            if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
-                moveInput = 1f;
-        }
-
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
-
-        if (transform.position.y < Camera.main.transform.position.y - fallThreshold)
-        {
-            gameManager.GameOver();
-        }
-
-        if (transform.position.x < wrapLeft)
-        {
-            transform.position = new Vector3(wrapRight, transform.position.y, transform.position.z);
-        }
-        else if (transform.position.x > wrapRight)
-        {
-            transform.position = new Vector3(wrapLeft, transform.position.y, transform.position.z);
-        }
+      gameManager.GameOver();
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    if (transform.position.x < wrapLeft)
     {
-        if (collision.gameObject.CompareTag("Platform"))
-        {
-            if (rb.linearVelocity.y <= 0f)
-            {
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-            }
-        }
+      transform.position = new Vector3(wrapRight, transform.position.y, transform.position.z);
     }
+    else if (transform.position.x > wrapRight)
+    {
+      transform.position = new Vector3(wrapLeft, transform.position.y, transform.position.z);
+    }
+
+    // 👇 Flip sprite based on direction
+    if (moveInput > 0)
+    {
+      sr.flipX = false; // facing right
+    }
+    else if (moveInput < 0)
+    {
+      sr.flipX = true; // facing left
+    }
+  }
+
+  void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (collision.gameObject.CompareTag("Platform"))
+    {
+      if (rb.linearVelocity.y <= 0f)
+      {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+      }
+    }
+  }
 }
